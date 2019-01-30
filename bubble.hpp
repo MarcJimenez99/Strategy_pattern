c11d29ec8851fbb1d1a9cc4afeae908a7f9c8b9a
MarcJimenez99 <46538808+MarcJimenez99@users.noreply.github.com>
2019-01-30 14:27:41 -0800

    Created an initial bubble.hpp

---
+++ bubble.hpp
@@ -0,0 +1,33 @@
+#ifndef _BUBBLE_HPP_
+#define _BUBBLE_HPP_
+
+#include "container.hpp"
+#include "sort.hpp"
+#include "ListContainer.hpp"
+
+// class Container;
+// class Sort;
+
+class Bubble : public Sort {
+    private:
+        Sort* sort_function = nullptr;
+    public:
+        void sort(Container* container) {
+            int i, j, flag = 1;
+            Base* temp;
+            int numLength = container->size();
+            for (i = 1; (i <=numLength) && flag; i++) {
+                flag = 0;
+                for (j = 0; j < (numLength - 1); j++) {
+                    if (container->at(j+1) < container->at(j)) {
+                        container->swap(j, j+1);
+                        flag = 1;
+                    }
+                }
+            }
+            return;
+        
+        }        
+};
+
+#endif
