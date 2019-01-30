0acfd2b6c216e1a7cda938f1f3d9b66ccb11ff2d
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-30 14:30:05 -0800

    Removed ListContainer.hpp

---
+++ ListContainer.hpp
@@ -0,0 +1,20 @@
+#ifndef _LIST_CONTAINER_HPP
+#define _LISt_CONTAINER_HPP
+
+#include "sort.hpp"
+#include "base.hpp"
+
+class Sort;
+class Base;
+
+class ListContainer {
+	protected:
+		Sort* sort_function;
+		list<Container*>'
+	public:
+		void set_sort_function(Sort* sort_function) {
+			//should point to an algorithm
+		}
+		void add_element(Base* element) {
+		
+		}
