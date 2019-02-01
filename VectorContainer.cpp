aa7c8166cef51d9700b0481eae01d5fc6fce0833
Neha Gupta <neha45556@gmail.com>
2019-01-31 19:39:23 -0800

    changed the hpp to cpp

--- VectorContainer.cpp
+++ VectorContainer.cpp
@@ -1,56 +1,44 @@
-#ifndef __VECTORCONTAINER_HPP__
-#define __VECTORCONTAINER_HPP__
-
 #include<iostream>
 #include<vector>
-#include "container.hpp"
-#include "base.h"
-#include "sort.hpp"
+#include "VectorContainer.h"
+
 
 using namespace std;
-
-class VectorContainer : public Container {
-        protected:
-            Sort * sort_function;
-            vector <Base *> v1;
-        public:
-            void set_sort_function(Sort * sort_function){
-                this->sort_function = sort_function;
-            }
-            
-            void add_element(Base * element){
+	
+void VectorContainer ::  set_sort_function(Sort * sort_function){
+	this->sort_function = sort_function;
+}
+           
+void VectorContainer :: add_element(Base * element){
                 v1.push_back(element);
-            }
+}
             
-            void print(){
-                for(int i = 0; i < v1.size(); ++i){
-                    (v1.at(i))->stringify();
-                }
-            }
+void VectorContainer :: print(){
+	for(int i = 0; i < v1.size(); ++i){
+        	(v1.at(i))->stringify();
+         }
+}
             
-            void sort(){
-                sort_function->sort(this);
-            }
+void VectorContainer :: sort(){
+        sort_function->sort(this);
+}
             
-            void swap(int i, int j){
-                Base* first = v1.at(i);
-		Base* second = v1.at(j);
-		v1.at(i) = second;
-		v1.at(j) = first;
-            }
+void VectorContainer ::  swap(int i, int j){
+	 Base* first = v1.at(i);
+	 Base* second = v1.at(j);
+	 v1.at(i) = second;
+	 v1.at(j) = first;
+}
             
-            Base * at(int i){
-		if (v1.size() == 0) {
-			return nullptr;
-		}
-                return v1.at(i);
-            }
+Base * VectorContainer ::  at(int i){
+	if (v1.size() == 0) {
+		return nullptr;
+	}
+	return v1.at(i);
+}
             
-            int size(){
-                return v1.size();
-            }
+int VectorContainer ::  size(){
+	return v1.size();
+}
             
-            
-};
-
-#endif
+           
