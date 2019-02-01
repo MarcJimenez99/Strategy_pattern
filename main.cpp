297445760c3a198d27248e60ded153bbf8c1b6b6
Marc Jimenez <marcjimenez999@gmail.com>
2019-01-31 18:26:04 -0800

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -53,34 +53,35 @@ int main() {
 
 	cout << "Second Example: " << endl;
 	
-	Op* seven = new Op(7);
+	Op* seven2 = new Op(7);
         Op* negOne = new Op(-1);
-        Mult* TreeA = new Mult(seven, negOne);
+        Mult* TreeAA = new Mult(seven2, negOne);
 
         Op* negThree = new Op(-3);
         Op* negFive = new Op(-5);
-        Add* TreeB = new Add(negThree, negFive);
+        Add* TreeAB = new Add(negThree, negFive);
 
-        Op* six = new Op(6);
+        Op* six6 = new Op(6);
         Op* eight = new Op(8);
-        Sub* TreeC = new Sub(six, eight);
+        Sub* TreeAC = new Sub(six6, eight);
 
-        VectorContainer* container = new VectorContainer();
+        VectorContainer* container2 = new VectorContainer();
 
-        container->add_element(TreeA);
-        container->add_element(TreeB);
-	container->add_element(TreeC);
+        container2->add_element(TreeAA);
+        container2->add_element(TreeAB);
+	container2->add_element(TreeAC);
 
-        cout << "First " << container->at(0)->evaluate() << endl;
-        cout << "Second " << container->at(1)->evaluate() << endl;
-        cout << "Third " << container->at(2)->evaluate() << endl;
+        cout << "First " << container2->at(0)->evaluate() << endl;
+        cout << "Second " << container2->at(1)->evaluate() << endl;
+        cout << "Third " << container2->at(2)->evaluate() << endl;
 
-        container->set_sort_function(new Bubble());
-        container->sort();
+        container2->set_sort_function(new SelectionSort());
+        container2->sort();
 
-        cout << "First " << container->at(0)->evaluate() << endl;
-        cout << "Second " << container->at(1)->evaluate() << endl;
-        cout << "Third " << container->at(2)->evaluate() << endl;ontainer->add_element(TreeC);
+        cout << "First " << container2->at(0)->evaluate() << endl;
+        cout << "Second " << container2->at(1)->evaluate() << endl;
+        cout << "Third " << container2->at(2)->evaluate() << endl;
+	
 
 	return 0;
 }
